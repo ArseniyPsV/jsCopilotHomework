c288f2d3e3ff4cfb4360baf579c2400cd5136d91
ArseniyPsV <157610525+ArseniyPsV@users.noreply.github.com>
2024-11-11 18:49:55 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,59 @@
+//  alert("I'm JavaScript");
+
+//  const constName = "Arseniy";
+//  let Name = constName;
+//  alert(Name);
+
+// let Name = "Джон";
+// let admin = Name;
+// alert(admin);
+
+// let planetName = "Earth";
+// let userName = "Jack";
+
+// const BIRTHDAY = '18.04.1982';
+// const age = someCode(BIRTHDAY);
+
+// let arr = ["яблоко", "банан"];
+// arr.push("апельсин", "киви");
+// alert(arr);
+
+// let arr = ["Алексей", "Мария"];
+// arr.push("Иван", "Ольга");
+// alert(arr);
+
+// let arr = [1, 2, 3, 4];
+// arr.pop();
+// alert(arr);
+
+// let arr = ["красный", "синий", "зеленый"];
+// arr.pop();
+// alert(arr);
+
+// let arr1 = ["Январь", "Февраль", "Март"];
+// let arr2 = ["Toyota", "BMW", "Audi"];
+// arr1.shift();
+// arr2.shift();
+// alert(arr1);
+// alert(arr2);
+
+// let arr1 = ["Картофель", "Морковь"];
+// let arr2 = ["Петр", "Анна"];
+// arr1.unshift("Огурец", "Помидор");
+// arr2.unshift("Дмитрий", "Елена");
+// alert(arr1);
+// alert(arr2);
+
+// let arr1 = ["Алекс", "Боб", "Чарли", "Дэвид", "Эдвард"];
+// let arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// arr1.splice(3, 2, "Фрэнк", "Гарри");
+// arr2.splice(3, 5, '10', '11', '12');
+// alert(arr1);
+// alert(arr2);
+
+let arr1 = ["Алекс", "Боб", "Лондон", "Берлин", "Рим"];
+let arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+arr1.slice(3, 2, "Фрэнк", "Гарри");
+arr2.slice(3, 5, '10', '11', '12');
+alert(arr1);
+alert(arr2);
