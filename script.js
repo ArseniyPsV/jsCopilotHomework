0761c2e67665445e3809984e8b4edca2191c3c1f
ArseniyPsV <157610525+ArseniyPsV@users.noreply.github.com>
2024-11-16 16:06:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,124 @@
-//  alert("I'm JavaScript");
+let fruits = ['яблоко', 'банан'];
+let students = ['Алексей', 'Мария'];
+fruits.push('апельсин', 'киви');
+students.push('Иван', 'Ольга');
+console.log(fruits);
+console.log(students);
 
-//  const constName = "Arseniy";
-//  let Name = constName;
-//  alert(Name);
+let numbers = [1, 2, 3, 4];
+let colors = ['красный', 'синий', 'зеленый'];
+let lastNumber = numbers.pop();
+let lastColor = colors.pop();
+console.log(numbers);
+console.log(lastNumber);
+console.log(colors);
+console.log(lastColor);
 
-// let Name = "Джон";
-// let admin = Name;
-// alert(admin);
+let months = ['Январь', 'Февраль', 'Март'];
+let cars = ['Toyota', 'BMW', 'Audi'];
+let firstMonth = months.shift();
+let firstCar = cars.shift();
+console.log(months);
+console.log(firstMonth);
+console.log(cars);
+console.log(firstCar);
 
-// let planetName = "Earth";
-// let userName = "Jack";
+let vegetables = ['картофель', 'морковь'];
+let conferenceMembers = ['Петр', 'Анна'];
+vegetables.unshift('огурец', 'помидор');
+conferenceMembers.unshift('Дмитрий', 'Елена');
+console.log(vegetables);
+console.log(conferenceMembers);
 
-// const BIRTHDAY = '18.04.1982';
-// const age = someCode(BIRTHDAY);
+let names = ['Алекс', 'Боб', 'Чарли', 'Дэвид', 'Эдвард'];
+let numbersForSplice = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+names.splice(2, 2, 'Фрэнк', 'Гарри');
+numbersForSplice.splice(2, 5, 10, 11, 12);
+console.log(names);
+console.log(numbersForSplice);
 
-// let arr = ["яблоко", "банан"];
-// arr.push("апельсин", "киви");
-// alert(arr);
+let cities = ['Минск', 'Москва', 'Париж', 'Лондон', 'Берлин', 'Рим'];
+let products = ['молоко', 'хлеб', 'сыр', 'масло', 'яйца'];
+let selectedCities = cities.slice(1, 4);
+let selectedProducts = products.slice(2, 5);
+console.log( selectedCities);
+console.log(selectedProducts);
 
-// let arr = ["Алексей", "Мария"];
-// arr.push("Иван", "Ольга");
-// alert(arr);
+let moreFruits = ['яблоко', 'банан'];
+let moreVegetables = ['морковь', 'помидор'];
+let food = moreFruits.concat(moreVegetables);
+let classAStudents = ['Иван', 'Мария'];
+let classBStudents = ['Дмитрий', 'Ольга'];
+let allStudents = classAStudents.concat(classBStudents);
+console.log(food);
+console.log(allStudents);
 
-// let arr = [1, 2, 3, 4];
-// arr.pop();
-// alert(arr);
+let numbersIndexOf = [1, 3, 7, 8, 7];
+let pets = ['собака', 'кошка', 'кот', 'попугай'];
+console.log(numbersIndexOf.indexOf(7));
+console.log(pets.indexOf('кошка'));
 
-// let arr = ["красный", "синий", "зеленый"];
-// arr.pop();
-// alert(arr);
+let citiesIncludes = ['Минск', 'Москва', 'Париж'];
+let Products = ['хлеб', 'молоко', 'масло'];
+console.log(citiesIncludes.includes('Минск'));
+console.log(Products.includes('молоко'));
 
-// let arr1 = ["Январь", "Февраль", "Март"];
-// let arr2 = ["Toyota", "BMW", "Audi"];
-// arr1.shift();
-// arr2.shift();
-// alert(arr1);
-// alert(arr2);
+let books = [
+  { title: 'Евгений Онегин', author: 'Пушкин' },
+  { title: 'Война и мир', author: 'Толстой' }
+];
+let numbersFind = [4, 9, 16, 25];
+console.log(books.find(book => book.author === 'Толстой'));
+console.log(numbersFind.find(num => num > 10));
 
-// let arr1 = ["Картофель", "Морковь"];
-// let arr2 = ["Петр", "Анна"];
-// arr1.unshift("Огурец", "Помидор");
-// arr2.unshift("Дмитрий", "Елена");
-// alert(arr1);
-// alert(arr2);
+let numbersFilter = [1, 2, 3, 4, 5, 6];
+let words = ['яблоко', 'банан', 'киви', 'манго'];
+console.log(numbersFilter.filter(num => num > 3));
+console.log(words.filter(word => word.length > 5));
 
-// let arr1 = ["Алекс", "Боб", "Чарли", "Дэвид", "Эдвард"];
-// let arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// arr1.splice(3, 2, "Фрэнк", "Гарри");
-// arr2.splice(3, 5, '10', '11', '12');
-// alert(arr1);
-// alert(arr2);
+let numbersFindIndex = [4, 9, 16, 25];
+let strings = ['кошка', 'собака', 'птица', 'крыса'];
+console.log(numbersFindIndex.findIndex(num => num > 10));
+console.log(strings.findIndex(str => str.length > 5));
 
-let arr1 = ["Алекс", "Боб", "Лондон", "Берлин", "Рим"];
-let arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-arr1.slice(3, 2, "Фрэнк", "Гарри");
-arr2.slice(3, 5, '10', '11', '12');
-alert(arr1);
-alert(arr2);
+let Names = ['Анна', 'Борис', 'Василий'];
+let numbersForEach = [1, 2, 3, 4, 5];
+names.forEach(Name => console.log(Name));
+numbersForEach.forEach(num => console.log(num));
+
+let numbersMap = [1, 2, 3, 4, 5];
+let stringsMap = ['яблоко', 'банан', 'киви'];
+console.log(numbersMap.map(num => num * num));
+console.log(stringsMap.map(str => str.length));
+
+let wordsSort = ['яблоко', 'банан', 'киви', 'манго'];
+let numbersSort = [5, 3, 8, 1, 2];
+console.log(wordsSort.sort());
+console.log(numbersSort.sort((a, b) => a - b));
+
+let numbersReverse = [1, 2, 3, 4, 5];
+let stringsReverse = ['яблоко', 'банан', 'киви', 'манго'];
+console.log(numbersReverse.reverse());
+console.log(stringsReverse.reverse());
+
+let stringToSplit = 'яблоко банан киви манго';
+let wordsToJoin = ['яблоко', 'банан', 'киви', 'манго'];
+console.log(stringToSplit.split(' '));
+console.log(wordsToJoin.join(', '));
+
+let numbersReduce = [1, 2, 3, 4, 5];
+let numbersReduce2 = [2, 3, 4];
+console.log(numbersReduce.reduce((sum, num) => sum + num, 0));
+console.log(numbersReduce2.reduce((prod, num) => prod * num, 1));
+
+let stringsSlice = ['яблоко', 'банан', 'киви', 'манго', 'папайя'];
+let citiesSlice = ['Минск', 'Москва', 'Париж', 'Лондон', 'Берлин', 'Рим'];
+console.log(stringsSlice.slice(1, 3));
+console.log(citiesSlice.slice(2, 5));
+
+let moreFruitsConcat = ['яблоко', 'груша'];
+let berries = ['малина', 'черника'];
+let animals = ['кошка', 'собака'];
+let birds = ['воробей', 'голубь'];
+console.log(moreFruitsConcat.concat(berries));
+console.log(animals.concat(birds));
